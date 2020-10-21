8f76fb40e5c3088b902726049123a92539ce6776
Greg Bair <gregbair@fastmail.fm>
2020-10-21 02:19:35 -0400

    Add more tests for PooledObjectInterceptor

--- test/ObjectPool.Tests/PooledObjectInterceptorTests.cs
+++ test/ObjectPool.Tests/PooledObjectInterceptorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Castle.DynamicProxy;
 using FluentAssertions;
 using Moq;
 using Xunit;
@@ -21,6 +22,50 @@ namespace ObjectPool.Tests
             var pool = new Mock<IObjectPool<IDisposable>>();
             Action act = () => new PooledObjectInterceptor<IDisposable>(pool.Object, null!);
             act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("actual");
+        }
+
+        [Fact]
+        public void InterceptThrowsNullInvocation()
+        {
+            var pool = new Mock<IObjectPool<IFoo>>();
+            var sut = new PooledObjectInterceptor<IFoo>(pool.Object, new Mock<IFoo>().Object);
+            Action act = () => sut.Intercept(null!);
+            act.Should().ThrowExactly<ArgumentNullException>().Which.ParamName.Should().Be("invocation");
+        }
+
+        [Fact]
+        public void InterceptRedirectsToObject()
+        {
+            var mockFoo = new Mock<IFoo>();
+            var pool = new Mock<IObjectPool<IFoo>>();
+
+            var generator = new ProxyGenerator();
+            var obj = generator.CreateInterfaceProxyWithTarget(mockFoo.Object,
+                new PooledObjectInterceptor<IFoo>(pool.Object, mockFoo.Object));
+
+            obj.DoThing("bar");
+
+            mockFoo.Verify(x=>x.DoThing("bar"), Times.Once);
+        }
+
+        [Fact]
+        public void InterceptDisposeRedirectsToPool()
+        {
+            var mockFoo = new Mock<IFoo>();
+            var pool = new Mock<IObjectPool<IFoo>>();
+
+            var generator = new ProxyGenerator();
+            var obj = generator.CreateInterfaceProxyWithTarget(mockFoo.Object,
+                new PooledObjectInterceptor<IFoo>(pool.Object, mockFoo.Object));
+
+            obj.Dispose();
+
+            pool.Verify(x => x.ReturnObject(mockFoo.Object), Times.Once);
+        }
+
+        public interface IFoo : IDisposable
+        {
+            void DoThing(string xyz);
         }
     }
 }
